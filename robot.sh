7be3b5b59e6a0502f42519aa190a6939effdae9b
ashishv-82 <110268624+ashishv-82@users.noreply.github.com>
2022-07-30 14:58:35 +1000

    Update robot.sh
    Added functions to validate input, and then calculated distance.

--- robot.sh
+++ robot.sh
@@ -1,37 +1,57 @@
 #!/bin/bash
- 
-forward_count=0
-backward_count=0
 
+check_input(){
+
+        
+        if [[ ! $1 =~ ['!@#$%^&*()_+'] ]]; then
+                echo "Yes"
+        else
+                echo "No"
+                exit 1;
+        fi
+}
+
+calculate_distance(){
+
+        forward_count=0
+        backward_count=0
+
+        #Set comma as delimiter
+        IFS=','
+ 
+        #Store the split steps into an array based on comma delimiter
+        read -a steparr <<< "$steps"
+ 
+        #Iterate through the array and search for Forward steps
+        for i in "${steparr[@]}";
+        do
+                if grep -q "F" <<< "$i" ;
+                then
+                        let forward_count+=$(echo "$i" | tr -dc '0-9')
+ 
+                elif grep -q "B" <<< "$i" ;
+                then
+                        let backward_count+=$(echo "$i" | tr -dc '0-9')
+                else
+                        :
+                fi
+        done
+ 
+        echo "Total forward Steps:" $forward_count
+        echo "Total backward Steps:" $backward_count
+ 
+        total_units=$(($forward_count - $backward_count))
+        echo "Total distance in Units:" ${total_units/-/}
+}
+
+clear
+
+ 
 #Get inputs from the user
 echo "Please provide steps to Mr. Robot and press ENTER."
 read steps
- 
+
+check_input $steps
+
 echo "Calculating..."
-
-#Set comma as delimiter
-IFS=','
-
-#Store the split steps into an array based on comma delimiter
-read -a steparr <<< "$steps"
-
-#Iterate through the array and search for Forward steps
-for i in "${steparr[@]}";
-do
-        if grep -q "F" <<< "$i" ;
-        then
-                let forward_count+=$(echo "$i" | tr -dc '0-9')
  
-        elif grep -q "B" <<< "$i" ;
-        then
-                let backward_count+=$(echo "$i" | tr -dc '0-9')
-        else
-                :
-        fi
-done
-
-echo "Total forward Steps:" $forward_count
-echo "Total backward Steps:" $backward_count
- 
-total_units=$(($forward_count - $backward_count))
-echo "Total distance in Units:" ${total_units/-/}
